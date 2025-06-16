e8a10cc3557a83bd139f0be3277e16c1fdd9c92c
ManprabhnoorKaur <manprabhnoor1404.becse24@chitkara.edu.in>
2025-06-16 21:10:35 +0530

    day3

---
+++ CodingDay3.cpp
@@ -0,0 +1,18 @@
+//array in cpp
+//loops in cpp
+
+#include <iostream>
+
+using namespace std;
+int main(){
+//     int arr[7]={1,2,3,4,6,7,8};
+//     for (int element: arr){
+//         cout << element << " "<<endl;
+//     }
+int arr[4]={1,2,3,4};
+int i=0;
+while(i<4){
+    cout << arr[i] << "  ";
+    i++;
+}
+}
