455d58a597d9f5e4c2738dea10a69361b7da854b
ManprabhnoorKaur <manprabhnoor1404.becse24@chitkara.edu.in>
2025-06-20 13:39:26 +0530

    all unmerged fol commited

--- CodingDay4.cpp
+++ CodingDay4.cpp
@@ -2,6 +2,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-
+int marks;
+ cin>>marks;
+        if(marks>=90){
+            cout<<"Grade A";
+        }
+        if(marks>=70){
+            cout<<"Grade B";
+        }if(marks>=50){
+            cout<<"Grade C";
+        }if(
+            marks>=35){
+            cout<<"Grade D";
+        }
+        else{
+            cout<<"Fail";
+        }
     return 0;
 }
---
+++ CodingDay5.cpp
@@ -0,0 +1,11 @@
+//2D array
+#include <iostream>
+using namespace std;
+int main() {
+int arr[3][3];
+arr[0][0]=1;
+arr[1][1]=2;
+arr[2][2]=3;
+cout<<arr[2][2]<<" ";
+    return 0;
+}
