6c2800abb509c2ca55e5249b029eedd6bd0d5b50
ManprabhnoorKaur <manprabhnoor1404.becse24@chitkara.edu.in>
2025-06-21 14:13:01 +0530

    day 5

--- CodingDay5.cpp
+++ CodingDay5.cpp
@@ -1,11 +1,16 @@
 //2D array
-#include <iostream>
+//strings
+#include <bits/stdc++.h>
 using namespace std;
 int main() {
-int arr[3][3];
-arr[0][0]=1;
-arr[1][1]=2;
-arr[2][2]=3;
-cout<<arr[2][2]<<" ";
+string s="striver";
+
+cout<<s[2];
+
     return 0;
 }
+//int arr[3][3];
+// arr[0][0]=1;
+// arr[1][1]=2;
+// arr[2][2]=3;
+// cout<<arr[2][2]<<" ";
