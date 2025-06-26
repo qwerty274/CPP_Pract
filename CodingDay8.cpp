9fc66f503e031c9ddb84ad828d395da368a52781
ManprabhnoorKaur <manprabhnoor1404.becse24@chitkara.edu.in>
2025-06-27 00:44:52 +0530

    7 patterns , day 10

---
+++ CodingDay8.cpp
@@ -0,0 +1,61 @@
+//all patterns practice with strivers sheet
+#include <iostream>
+using namespace std;
+void print1(int n){
+for(int i=0;i<=n;i++){
+    for(int j=0;j<n;j++){
+        cout<<"* ";
+    }
+    cout<<endl;
+}
+};
+void print2(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+};
+void print3(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j <<"  ";
+       }
+        cout<<endl;
+    }
+};
+void print4(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+};
+void print5(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=i;j<n;j++){
+            cout<<"*  ";
+        }
+        cout<<endl;
+    }
+}
+void print6(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            cout<< j<<" ";
+        }
+        cout<<endl;
+    }
+}
+void print7(int n){
+    
+}
+int main() {
+int n;
+cout<<"enter n: ";
+cin>>n;
+print7(n);
+    return 0;
+}
