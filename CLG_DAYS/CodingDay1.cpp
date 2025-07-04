73110ebc1dec93f6bbf4957de6660f69060af7de
ManprabhnoorKaur <manprabhnoor1404.becse24@chitkara.edu.in>
2025-07-04 18:05:30 +0530

    clg day 2 of coding

---
+++ CLG_DAYS/CodingDay1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a;
+int b;
+cout<<"enter A and B:"<<" ";
+cin>>a>>b;
+
+
+cout<<"sum of A and B is "<< a+b<<endl;
+    return 0;
+}
---
+++ CLG_DAYS/CodingDay2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //loops
+    for(int i=0;i<=10;++i){
+    cout<<i<<endl;
+    }
+    // int a=2;
+    // cout<<"value  of a before update: "<<a<<endl;
+    // // a++;
+    // cout<<"value  of a after update: "<<a++<<endl;
+    //  cout<<"value  of at last: "<<++a;
+    return 0;
+}
